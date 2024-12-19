54395e76c6fe8c7da7dd21c9b6068e405ebcbc52
111Firebird777 <rahmatullaevdierbek481@gmail.com>
2024-12-19 22:59:32 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.scss";
+import React from "react";
+import Hero from "./components/Hero/Hero";
+import OurServicecards from "./components/OurServiceCards/OurServicecards";
+import OurServicetext from "./components/OurServicetext/OurServicetext";
+
+const App = () => {
+  return (
+    <>
+     
